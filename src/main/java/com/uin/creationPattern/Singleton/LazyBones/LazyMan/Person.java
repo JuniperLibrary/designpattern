c93e433ac9a4a347542c3405ae910998289ba46f
wanglufei <2686221966@qq.com>
2022-04-25 20:34:43 +0800

    完善文档

--- src/main/java/com/uin/creationPattern/Singleton/LazyBones/LazyMan/Person.java
+++ src/main/java/com/uin/creationPattern/Singleton/LazyBones/LazyMan/Person.java
@@ -1,26 +1,28 @@
 package com.uin.creationPattern.Singleton.LazyBones.LazyMan;
 
 /**
- * \* Created with IntelliJ IDEA.
- * \* @author wanglufei
- * \* Date: 2021年08月27日 14:59
- * \* Description: 第一种方法 懒汉(线程不安全)
- * \
+ * 第一种方法 懒汉(线程不安全)
  */
 public class Person {
 
     private String name;
     private int age;
 
-    //懒汉--需要的时候在创建对象
+    /**
+     * 懒汉--需要的时候在创建对象
+     */
     private static Person instance;
 
-    //构造器私有，外部不能实例化
+    /**
+     * 构造器私有，外部不能实例化
+     */
     private Person() {
 
     }
 
-    //提供一个静态的公有方法，当使用该方法时才去创建instance
+    /**
+     * 提供一个静态的公有方法，当使用该方法时才去创建instance
+     */
     public static Person wanglufeiPerson() {
         //没有在去创建
         if (instance == null) {
