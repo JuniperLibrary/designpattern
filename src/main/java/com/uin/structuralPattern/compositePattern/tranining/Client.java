202e1b467c97c574ad127595ecf7e7b70f118062
lin.liu <lin.liu@quantinfotech.com>
2024-07-23 14:41:08 +0800

    feat:组合模式 & 透明组合模式 & 安全组合模式 & 练习

---
+++ src/main/java/com/uin/structuralPattern/compositePattern/tranining/Client.java
@@ -0,0 +1,28 @@
+package com.uin.structuralPattern.compositePattern.tranining;
+
+public class Client {
+
+  public static void main(String[] args) {
+    // 创建单元控件
+    Component button1 = new Button("Submit");
+    Component button2 = new Button("Cancel");
+    Component textBox = new TextBox("Enter text here...");
+
+    // 创建容器控件
+    Container form = new Container();
+    form.add(button1);
+    form.add(button2);
+    form.add(textBox);
+
+    // 创建子容器
+    Container panel = new Container();
+    panel.add(new Button("OK"));
+    panel.add(new TextBox("Enter your name"));
+
+    // 添加子容器到容器控件
+    form.add(panel);
+
+    // 绘制整个界面
+    form.draw();
+  }
+}
---
+++ src/main/java/com/uin/structuralPattern/compositePattern/transparentCompositePattern/Client.java
@@ -0,0 +1,21 @@
+package com.uin.structuralPattern.compositePattern.transparentCompositePattern;
+
+public class Client {
+
+  public static void main(String[] args) {
+    Graphic circle = new Circle();
+    Graphic rectangle = new Rectangle();
+
+    CompositeGraphic composite = new CompositeGraphic();
+    composite.add(circle);
+    composite.add(rectangle);
+
+    CompositeGraphic subComposite = new CompositeGraphic();
+    subComposite.add(new Circle());
+    subComposite.add(new Rectangle());
+
+    composite.add(subComposite);
+
+    composite.draw();
+  }
+}
